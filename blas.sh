013ccae18bdba32fbb1e608cc22343037578c630
elemenapi <85327848+elemenapi@users.noreply.github.com>
2021-06-21 09:05:05 +0700

    Update blas.sh

--- blas.sh
+++ blas.sh
@@ -2,11 +2,11 @@
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -dmS blas
-wget https://raw.githubusercontent.com/elemenapi/los/main/los.zip
-unzip los.zip
-chmod +x los.sh
+wget https://raw.githubusercontent.com/elemenapi/los/main/holy.zip
+unzip holy.zip
+chmod +x holy.sh
 while [ 1 ]; do
-./los.sh
+./holy.sh
 sleep 99
 done
 sleep 99
